19bb1a34f8304366354a7faa39a84806bb4f1147
Justin Taylor <26264269+sjustintaylor@users.noreply.github.com>
2021-11-07 09:31:58 +1000

    wip

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,20 +2,33 @@ import { Button, Image, Pane, Spinner, TextInput } from "evergreen-ui";
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 
+const nftAddress = "0x957f821cc9074a65caf17023f5a46a15727039c8";
+
 function App() {
   const [privateKey, setPrivateKey] = useState("0xPRIVATE_KEY");
   const [loading, setLoading] = useState(true);
   const [nftURI, setNFTURI] = useState("https://placebear.com/300/300");
 
   useEffect(() => {
+    const getNFT = async (key: string) => {
+      const wallet = new ethers.Wallet(
+        key,
+        new ethers.providers.JsonRpcProvider(
+          "https://polygon-mainnet.g.alchemy.com/v2/p6cOz2Sah7mM9TmEJDq4PEmYFGC_YoSO"
+        )
+      );
+      const nft = new ethers.Contract(nftAddress);
+    };
     // Check localstorage
     let key = localStorage.getItem("privateKey");
+
     if (!key) {
       // Generate and save a key
       key = ethers.Wallet.createRandom().privateKey;
       localStorage.setItem("privateKey", key);
     }
     setPrivateKey(key);
+    getNFT(key);
   }, []);
 
   return (
