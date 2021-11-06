45e9a62f9868bbc7b3bab1ce0c4e8d0d9cc188b6
Aleksandar Gujas <alex.g@labrys.io>
2021-11-06 12:16:57 +1000

    get rugd

--- contracts/Ruggable.sol
+++ contracts/Ruggable.sol
@@ -5,32 +5,66 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract OrdinaryNFT is ERC721, ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    uint256 const public PRICE = 3 ether;
 
-    constructor() public ERC721("Ruggable", "RUG") {
+    Counters.Counter private _tokenIdCounter;
 
-    }
+    constructor() ERC721("OrdinaryNFT", "ONT") {}
 
-    function mintNFT(address recipient, string memory tokenURI)
-        public onlyOwner
-        returns (uint256)
-    {
-        _tokenIds.increment();
-
-        uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
+    function safeMint(address to, string tokenURI) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
         _setTokenURI(newItemId, tokenURI);
-
-        return newItemId;
     }
 
-    function purchaseNFT() external payable returns (uint256) {
+
+    function purchase(uint256 nftId) external payable returns (uint256) {
+        // has to be minted
+        require(nftId < _tokenIdCounter.current());
+        // have to pay enough
         require(
-            msg.value >= _nftSales[nftId].price,
+            msg.value >= PRICE,
             "value less than price of nft"
         );
-        _purchaseArtwork(nftId);
+        // has to not already be purchased
+        require(ownerOf(nftId) == address(this));
+        // then you can buy it
+        _purchase(nftId);
     }
+
+    function _purchase(uint256 nftId) private {
+        address payable buyer = msg.sender;
+        ERC721(address(this))).transferFrom(address(this), buyer, nftId);
+    }
+
+    
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+    
 }
