b92345eb1561256ddb91ff403416b1fbc00a30c4
Aleksandar Gujas <alex.g@labrys.io>
2021-11-06 12:41:57 +1000

    copy ruggable into rorschach for now

--- contracts/Rorshach.sol
+++ contracts/Rorshach.sol
@@ -1,22 +1,75 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Rorschach {
-    string private greeting;
+contract Rorschach is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    uint256 public constant PRICE = 3 ether;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("Rorschach", "RCH") {}
+
+    function safeMint(address to, string memory tokenURI) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
+
+    function purchase(uint256 nftId) external payable returns (uint256) {
+        // has to be minted
+        require(nftId < _tokenIdCounter.current());
+        // have to pay enough
+        require(
+            msg.value >= PRICE,
+            "value less than price of nft"
+        );
+        // has to not already be purchased
+        require(ownerOf(nftId) == address(this));
+        // then you can buy it
+        _purchase(nftId);
     }
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    function _purchase(uint256 nftId) private {
+        address payable buyer = payable(msg.sender);
+        ERC721(address(this)).transferFrom(address(this), buyer, nftId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 }
